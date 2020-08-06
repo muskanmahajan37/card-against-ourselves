893e6f2614610944cb77479352e109cfb7a1b362
Domikado5 <dominik.sawicki52@gmail.com>
2020-08-06 19:24:43 +0200

    Made views for decks

--- cards/decks/views.py
+++ cards/decks/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Deck, WhiteCard, BlackCard
+
 def creator(request):
     return render(request, 'decks/creator.html')
 
 def decks(request):
-    return render(request, 'decks/decks.html')
+
+    decks = Deck.objects.all()
+
+    context = {
+        'decks': decks
+    }
+
+    return render(request, 'decks/decks.html', context)
