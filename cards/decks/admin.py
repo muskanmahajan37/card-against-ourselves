ccdcf9db1a23608cbdacf02aceae0a2d34c3bb1f
Domikado5 <dominik.sawicki52@gmail.com>
2020-08-06 19:24:18 +0200

    Added models on deck app

--- cards/decks/admin.py
+++ cards/decks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Deck, WhiteCard, BlackCard
+
 # Register your models here.
+
+admin.site.register(Deck)
+
+admin.site.register(WhiteCard)
+
+admin.site.register(BlackCard)
--- cards/decks/models.py
+++ cards/decks/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Deck(models.Model):
+    name = models.CharField(max_length=120)
+    author = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+class WhiteCard(models.Model):
+    text = models.CharField(max_length=120)
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name='white_cards')
+
+    def __str__(self):
+        return self.deck.name + ' - ' + self.text
+
+class BlackCard(models.Model):
+    text = models.CharField(max_length=120)
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name='black_cards')
+
+    def blank_count(self):
+        return str(self.text).count('_')
+
+    def __str__(self):
+        return self.deck.name + ' - ' + self.text
