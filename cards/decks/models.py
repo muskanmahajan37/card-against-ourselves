bb9c7590a82fab1c9cad09f516ffd97bb174a2c2
Domikado5 <dominik.sawicki52@gmail.com>
2020-08-07 19:15:44 +0200

    Added deck form

--- cards/decks/models.py
+++ cards/decks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Deck(models.Model):
     name = models.CharField(max_length=120)
-    author = models.CharField(max_length=120)
+    author = models.CharField(max_length=120, default='dodo')
 
     def __str__(self):
         return self.name
