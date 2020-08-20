6bb62a930a50c684be662a898d9fbd46101c022b
Domikado5 <dominik.sawicki52@gmail.com>
2020-08-20 13:39:27 +0200

    Added staticfiles

--- cards/cards/settings.py
+++ cards/cards/settings.py
@@ -126,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR + "/staticfiles/",
+]
