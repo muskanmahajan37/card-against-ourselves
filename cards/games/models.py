0c3735767341f3917e7ccba06579b96cb0634e2b
Domikado5 <dominik.sawicki52@gmail.com>
2020-08-28 20:05:38 +0200

    Updated games model

--- cards/games/models.py
+++ cards/games/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from decks.models import Deck
+from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import datetime
 # Create your models here.
 
 class Game(models.Model):
     author = models.CharField(max_length=80, default='Dodo')
     password = models.CharField(max_length=80)
+    max_users = models.IntegerField(validators=[MinValueValidator(2), MaxValueValidator(10)])
+    # users = models.ForeignKey()
+    decks = models.ManyToManyField(Deck)
+    created = models.DateTimeField(default=datetime.utcnow())
+
+    def __str__(self):
+        return self.author + '\'s ' + 'Game'
