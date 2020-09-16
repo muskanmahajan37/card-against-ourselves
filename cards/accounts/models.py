40dbc756ae8fd4aac26361ed4fb126eb73e3d187
HulewiczKamil <hulewicz.k@gmail.com>
2020-09-16 17:23:30 +0200

    Testing bash commands

--- cards/accounts/models.py
+++ cards/accounts/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Account(models.Model):
     name = models.CharField(
         max_length=50,
+        null=False,
+        default='User',
+        blank=False
     )
     email = models.CharField(
         max_length=100,
+        null=False,
+        blank=False,
+        default='aaa'
     )
     join_date = models.DateTimeField(
         auto_now_add=True,
@@ -14,3 +20,4 @@ class Account(models.Model):
     )
     def __str__(self):
         return self.name
+    #xd
