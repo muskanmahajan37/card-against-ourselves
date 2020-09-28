251c4e10dafa84fd667c4d92f0b6e10f82463cd2
HulewiczKamil <hulewicz.k@gmail.com>
2020-09-28 23:51:00 +0200

    login and registation

--- cards/accounts/urls.py
+++ cards/accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
    path('register/', views.register, name='register'),
-   path('login/', views.login, name='login'),
+   path('login/', views.user_login_authentication, name='login'),
 ]
--- cards/accounts/views.py
+++ cards/accounts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
+
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 from .forms import CreateUserForm
@@ -13,9 +18,28 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            user = form.cleaned_data.get('username')
+            messages.success(request, f'Account was created for {user}')
+            return redirect('login')
+        else:
+            form = CreateUserForm()
+            
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
-def login(request):
-    pass
+def user_login_authentication(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('creator')
+        else:
+            messages.info(request, 'username/password is incorrect')
+
+    context_view = {}
+    return render(request, 'accounts/login.html', context_view)
